af0d64a42330cadd37403949b67a577c0daec67f
Dmitriy <dmitriy.kolokot@gmail.com>
2020-12-24 13:46:15 +0200

    docker

--- mongo/init-mongo.js
+++ mongo/init-mongo.js
@@ -1,14 +1,30 @@
 print('Start #################################################################');
 
-db = db.getSiblingDB('gamembo');
+print('db mediator');
+
+db = db.getSiblingDB('mediator');
 
 db.createUser({
-        user : 'foo',
-        pwd  : 'foo',
+    user : 'mediator',
+    pwd  : 'h&4B5=FgvV;D]hyj',
+    roles: [
+        {
+            role: 'readWrite',
+            db  : 'mediator'
+        }
+    ]
+});
+
+print('db backoffice - use it for BO');
+
+db = db.getSiblingDB('backoffice');
+db.createUser({
+        user : 'backoffice',
+        pwd  : 'sGq%`6U+k?+-2)K~',
         roles: [
             {
                 role: 'readWrite',
-                db  : 'gamembo'
+                db  : 'backoffice'
             }
         ]
 });
